202a027ff4e9efa141872bc2ae976955255b5638
Adrian Gjerstad <adgjerstad@gmail.com>
2019-10-27 21:49:07 +0000

    Finished bash startup script

--- webpy/webpy.sh
+++ webpy/webpy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function webpy() {
-  command python3 > /dev/null 2>&1
+  command python3 -V > /dev/null 2>&1
 
   if [[ $? -eq 127 ]]; then
+    export WEBPY_PATH=''
     echo 'Python3 is required to run WebPy. Please download Python3. Aborting...'
     return 150
   fi
@@ -13,10 +14,11 @@ function webpy() {
     read WEBPY_PATH
 
     if [[ $WEBPY_PATH != '/'* ]]; then
-      WEBPY_PATH='${PWD}/${WEBPY_PATH}'
+      WEBPY_PATH=${PWD}/${WEBPY_PATH}
     fi
 
     if [[ ! -d $WEBPY_PATH ]]; then
+      export WEBPY_PATH=''
       echo 'That is not a path that exists.'
       return 152
     fi
@@ -25,7 +27,11 @@ function webpy() {
   fi
 
   if [[ $WEBPY_PATH == *'/' ]]; then
-    lengthm1=${${#WEBPY_PATH}-1}
-    echo $WEBPY_PATH | cut -c-$lengthm1
+    WEBPY_PATH=${WEBPY_PATH:0:-1}
+    export WEBPY_PATH
   fi
+
+  python3 $WEBPY_PATH/webpy.py $@
+
+  return $?
 }
